8733c777b31f10c356b25524deea81b469b8388b
Oscar Ivan Olivares Carsi <99694349+Carsiano@users.noreply.github.com>
2023-06-16 16:37:19 -0600

    update singin

--- Pages/SingIn.cshtml.cs
+++ Pages/SingIn.cshtml.cs
@@ -56,9 +56,9 @@ public class SingInModel : PageModel
     {   
         if(activeButton == "ACADEMIC"){
             bool withoutNullFields = ValidateNullFieldsAcademic();
-            bool correctFields = ValidateFieldsAcademic();
             if (withoutNullFields)
             {
+                bool correctFields = ValidateFieldsAcademic();
                 if (correctFields)
                 {
                     Credentials credentialsAcademic = new Credentials();
@@ -86,42 +86,49 @@ public class SingInModel : PageModel
                                 if (responseUser.IsSuccessStatusCode)
                                 {
                                     Message ="Usuario creado exitosamente, inicia sesión para ingresar";
+                                    clearFields();
                                 }
                                 else
                                 {
                                     Message ="No se pudo crear tu usuario, inténtalo más tarde";
+                                    clearFields();
                                 }
                             }
                             else
                             {
                                 Message = "No se pudieron crear tus credenciales, inténtalo más tarde";
+                                clearFields();
                             }
                         }
                         else
                         {
                             Message = "Nombre de usuario en uso, ingresa otro nombre de usuario";
+                            clearFields();
                         }
                     }
                     else
                     {
                         Message = "Ya tienes una cuenta en este sistema, ve al inicio de sesión e ingresa tus credenciales de acceso";
+                        clearFields();
                     }
                 }
                 else
                 {
                     Message = "Verifica los datos proporcionados";
+                    clearFields();
                 }
             }
             else
             {
                 Message = "No puedes dejar campos vacíos";
+                clearFields();
             }
         }
         else{
             bool withoutNullFields = ValidateNullFieldsStudents();
-            bool correctFields = ValidateFieldsStudent();
             if (withoutNullFields)
             {
+            bool correctFields = ValidateFieldsStudent();
                 if (correctFields)
                 {
                     emailStudent = "z" + studentSchoolId.Substring(0, 1).ToLower() + studentSchoolId.Substring(1, 8) + "@estudiantes.uv.mx";
@@ -152,107 +159,131 @@ public class SingInModel : PageModel
                                 if (responseUser.IsSuccessStatusCode)
                                 {
                                     Message = "Usuario creado exitosamente, inicia sesión para ingresar";
+                                    clearFields();
                                 }
                                 else
                                 {
                                     Message = "No se pudo crear tu usuario, inténtalo más tarde";
+                                    clearFields();
                                 }
                             }
                             else
                             {
                                 Message = "No se pudieron crear tus credenciales, inténtalo más tarde";
+                                clearFields();
                             }
                         }
                         else
                         {
                             Message = "Nombre de usuario en uso, ingresa otro nombre de usuario";
+                            clearFields();
                         }
                     }
                     else
                     {
                         Message ="Ya tienes una cuenta en este sistema, ve al inicio de sesión e ingresa tus credenciales de acceso";
+                        clearFields();
                     }
                 }
                 else
                 {
                     Message ="Verifica los datos proporcionados";
+                    clearFields();
                 }
             }
             else
             {
                 Message = "No puedes dejar campos vacíos";
+                clearFields();
             }
         }
         return Page();
 
     }
     public bool ValidateSpecialCharacter(string verify)
+    {
+        bool withoutSpecialCharacter = true;
+        string specialCharacteres = "*#+-_;.@%&/()=!?¿¡{}[]^<>";
+        foreach (char character in verify)
         {
-            bool withoutSpecialCharacter = true;
-            string specialCharacteres = "*#+-_;.@%&/()=!?¿¡{}[]^<>";
-            foreach (char character in verify)
+            if (character >= '0' && character <= '9')
             {
-                if (character >= '0' && character <= '9')
-                {
-                    withoutSpecialCharacter = false;
-                }
+                withoutSpecialCharacter = false;
             }
-            foreach (char character in verify)
+        }
+        foreach (char character in verify)
+        {
+            foreach (char specialCharacter in specialCharacteres)
             {
-                foreach (char specialCharacter in specialCharacteres)
+                if (character == specialCharacter)
                 {
-                    if (character == specialCharacter)
-                    {
-                        withoutSpecialCharacter = true;
-                    }
-
+                    withoutSpecialCharacter = true;
                 }
 
-                if (withoutSpecialCharacter)
-                {
-                    break;
-                }
             }
-            return withoutSpecialCharacter;
-        }
-        public bool ValidateFieldsStudent()
-        {
-            bool correctField = false;
-            correctField = studentSchoolId.Length == 9;
-            correctField = correctField && studentSchoolId.Substring(0, 1) == "S";
-            correctField = correctField && ValidateSpecialCharacter(studentName);
-            correctField = correctField && ValidateSpecialCharacter(studentPaternalSurname);
-            correctField = correctField && ValidateSpecialCharacter(studentMaternalSurname);
-            return correctField;
-        }
-        public bool ValidateFieldsAcademic()
-        {
-            bool correctField = false;
-            correctField = EmailValidator.Validate(academicEmail);
-            correctField = correctField && ValidateSpecialCharacter(academicName);
-            correctField = correctField && ValidateSpecialCharacter(academicPaternalSurname);
-            correctField = correctField && ValidateSpecialCharacter(academicMaternalSurname);
-            correctField = correctField && academicEmail.EndsWith("@uv.mx", StringComparison.OrdinalIgnoreCase);
-            return correctField;
-        }
-        public bool ValidateNullFieldsAcademic()
-        {
-            bool fullFields = false;
-            if (!String.IsNullOrWhiteSpace(academicName) && !String.IsNullOrWhiteSpace(academicPaternalSurname) && !String.IsNullOrWhiteSpace(academicMaternalSurname) && !String.IsNullOrWhiteSpace(academicEmail) && !String.IsNullOrWhiteSpace(academicPassword) && !String.IsNullOrWhiteSpace(academicUsername))
-            {
-                fullFields = true;
-            }
-            return  fullFields;
-        }
-        public bool ValidateNullFieldsStudents()
-        {
-            bool fullFields = false;
-            if (!String.IsNullOrWhiteSpace(studentName) && !String.IsNullOrWhiteSpace(studentPaternalSurname) && !String.IsNullOrWhiteSpace(studentMaternalSurname) && !String.IsNullOrWhiteSpace(studentSchoolId) && !String.IsNullOrWhiteSpace(studentPassword))
-            {
-                fullFields = true;
-            }
-            return fullFields;
-        }
 
+            if (withoutSpecialCharacter)
+            {
+                break;
+            }
+        }
+        return withoutSpecialCharacter;
+    }
+    public bool ValidateFieldsStudent()
+    {
+        bool correctField = false;
+        correctField = studentSchoolId.Length == 9;
+        correctField = correctField && studentSchoolId.Substring(0, 1) == "S";
+        correctField = correctField && ValidateSpecialCharacter(studentName);
+        correctField = correctField && ValidateSpecialCharacter(studentPaternalSurname);
+        correctField = correctField && ValidateSpecialCharacter(studentMaternalSurname);
+        return correctField;
+    }
+    public bool ValidateFieldsAcademic()
+    {
+        bool correctField = false;
+        correctField = EmailValidator.Validate(academicEmail);
+        correctField = correctField && ValidateSpecialCharacter(academicName);
+        correctField = correctField && ValidateSpecialCharacter(academicPaternalSurname);
+        correctField = correctField && ValidateSpecialCharacter(academicMaternalSurname);
+        correctField = correctField && academicEmail.EndsWith("@uv.mx", StringComparison.OrdinalIgnoreCase);
+        return correctField;
+    }
+    public bool ValidateNullFieldsAcademic()
+    {
+        bool fullFields = false;
+        if (!String.IsNullOrWhiteSpace(academicName) && !String.IsNullOrWhiteSpace(academicPaternalSurname) && !String.IsNullOrWhiteSpace(academicMaternalSurname) && !String.IsNullOrWhiteSpace(academicEmail) && !String.IsNullOrWhiteSpace(academicPassword) && !String.IsNullOrWhiteSpace(academicUsername))
+        {
+            fullFields = true;
+        }
+        return  fullFields;
+    }
+    public bool ValidateNullFieldsStudents()
+    {
+        bool fullFields = false;
+        if (!String.IsNullOrWhiteSpace(studentName) && !String.IsNullOrWhiteSpace(studentPaternalSurname) && !String.IsNullOrWhiteSpace(studentMaternalSurname) && !String.IsNullOrWhiteSpace(studentSchoolId) && !String.IsNullOrWhiteSpace(studentPassword))
+        {
+            fullFields = true;
+        }
+        return fullFields;
+    }
+
+    public void clearFields()
+    {
+        studentName = "" ;
+        studentPaternalSurname = "";
+        studentMaternalSurname = "";
+        studentSchoolId = "";
+        studentEducationalProgram = "";
+        studentUsername = "";
+        studentPassword = "";
+        academicName = "";
+        academicPaternalSurname = "";
+        academicMaternalSurname = "";
+        academicEmail = "";
+        academicUsername = "";
+        academicPassword = "";
+        emailStudent = "";
+    }
     
 }
